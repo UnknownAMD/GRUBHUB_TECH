d69c6977e3fee8d88760bff757ad5ac8ccf32cc9
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-07-21 21:34:41 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -48,4 +48,3 @@ local Window = UILibrary.new("GrubHub V6 ~ The Wild West", 5013109572)
 local Repository  = "https://github.com/botdevXD/GRUBHUB_TECH"
 local GamesPath = Repository .. "/Games/"
 
-s
