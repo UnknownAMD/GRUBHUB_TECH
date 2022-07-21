81d3d5b8fd68feb295e3332e3ebd9015b7a17bcb
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-07-21 21:31:23 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,50 @@
+getgenv()["USE_GRUBHUB_UNIVERSAL"] = true
+
+local Settings_Name = nil
+local Window = nil
+local SettingsPage = nil
+local SettingsSection = nil
+
+local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/GrubHub/main/modules/VenyXUI.lua", true))()
+
+if not isfolder("grubhub_v6_bin") then
+   makefolder("grubhub_v6_bin")
+end
+
+local function FixName(GameName)
+   return GameName:gsub("[%s%p%d%c%z]", "")
+end
+
+local function GetGameConfig(GameName)
+   local Table = {}
+   if isfolder("grubhub_v6_bin") then
+         if isfile("grubhub_v6_bin/" .. tostring(GameName)) then
+            local HttpServices = game:GetService("HttpService")
+            local ConfigContents = readfile("grubhub_v6_bin/" .. tostring(GameName))
+            local DecodedSuccess, DecodedContents = pcall(HttpServices.JSONDecode, HttpServices, tostring(ConfigContents))
+            if DecodedSuccess then
+               Table = DecodedContents
+            end
+         end
+   end
+   return Table
+end
+
+local function SaveGameConfig(GameName, ConfigTable)
+   if isfolder("grubhub_v6_bin") then
+         local HttpServices = game:GetService("HttpService")
+         local EncodedSuccess, EncodedContents = pcall(HttpServices.JSONEncode, HttpServices, ConfigTable)
+
+         if EncodedSuccess then
+            writefile("grubhub_v6_bin/" .. tostring(GameName), tostring(EncodedContents))
+            return true
+         end
+   end
+   return false
+end
+
+local Window = UILibrary.new("GrubHub V6 ~ The Wild West", 5013109572)
+
+local Repository  = "https://github.com/botdevXD/GRUBHUB_TECH"
+local GamesPath = Repository .. "/Games/"
+
