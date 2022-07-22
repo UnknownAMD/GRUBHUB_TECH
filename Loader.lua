5a478d01038c0bb8a508333020ec02548eb6e157
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-07-22 23:05:30 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -108,6 +108,6 @@ end)
 local queue_on_teleport = type(syn) == "table" and syn.queue_on_teleport or queue_on_teleport
 
 if queue_on_teleport ~= nil then
-    Source = "task.wait(15)\n" .. Source
+    Source = "task.wait(35)\n" .. Source
     queue_on_teleport(Source)
 end
