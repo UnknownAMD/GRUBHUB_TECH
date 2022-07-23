4faee1f4ab467bace3540953384b727fc3e496d9
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-07-23 04:44:44 +0100

    Update ServerCycler.lua

--- Games/ServerCycler.lua
+++ Games/ServerCycler.lua
@@ -18,7 +18,7 @@ local OldServersName = Format(GamePlaceServersFolder, game.placeId .. "_OldServe
 local API_URL = "https://games.roblox.com/v1/games/%s/servers/Public?limit=%s%s"
 
 local OldServerData = {
-    ExpireTime = 60 * 2,
+    ExpireTime = 60 * 5,
     Time = tick(),
     ServerIds = {}
 }
