158880679afb74077bd12917d4f982cfdaef185e
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-08-02 02:41:55 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local RawPath = "https://raw.githubusercontent.com/botdevXD/GRUBHUB_TECH/main/Ga
 
 local Games = {
     {GameName = "ServerCycler.lua"},
-    --{GameName = "ESP_MODULE.lua"},
+    {GameName = "ESP_MODULE.lua"},
     {GameName = "The_Wild_West.lua"},
     {GameName = "PetSimX.lua"},
     {GameName = "Universal.lua"}
