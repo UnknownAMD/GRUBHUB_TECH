23c3ca536959a0de49934eeaa0756fc2d643aed9
_Ben <54240316+botdevXD@users.noreply.github.com>
2024-06-19 17:17:49 +0100

    Update CG_LOW_GFX_MODULE.lua

--- CG_LOW_GFX_MODULE.lua
+++ CG_LOW_GFX_MODULE.lua
@@ -8,7 +8,8 @@ end
 
 table.clear(shared.CG_LOW_GFX_CONNECTIONS)
 
-shared.CG_LOW_GFX_CACHE = shared.CG_LOW_GFX_CACHE or {}
+shared.CG_LOW_GFX_LIGHTING_CACHE = shared.CG_LOW_GFX_LIGHTING_CACHE or {}
+shared.CG_LOW_GFX_PART_CACHE = shared.CG_LOW_GFX_PART_CACHE or {}
 
 local function getProperty(obj, propName)
     local success, value = pcall(function()
@@ -24,11 +25,11 @@ local function setProperty(obj, propName, value)
     end)
 end
 
-local function addToCache(Part)
+local function addToPartCache(Part)
     if typeof(Part) ~= "Instance" or not Part:IsA("BasePart") then return end
-    if shared.CG_LOW_GFX_CACHE[Part] ~= nil then return end
+    if shared.CG_LOW_GFX_PART_CACHE[Part] ~= nil then return end
 
-    shared.CG_LOW_GFX_CACHE[Part] = {
+    shared.CG_LOW_GFX_PART_CACHE[Part] = {
         Material = getProperty(Part, "Material"),
         Reflectance = getProperty(Part, "Reflectance"),
         CastShadow = getProperty(Part, "CastShadow")
@@ -38,25 +39,25 @@ end
 for _, Part in pairs(workspace:GetDescendants()) do
     if not Part:IsA("BasePart") then continue end
 
-    addToCache(Part)
+    addToPartCache(Part)
 end
 
 table.insert(shared.CG_LOW_GFX_CONNECTIONS, workspace.DescendantAdded:Connect(function(Part)
     if not Part:IsA("BasePart") then return end
 
-    addToCache(Part)
+    addToPartCache(Part)
 end))
 
 table.insert(shared.CG_LOW_GFX_CONNECTIONS, workspace.DescendantRemoving:Connect(function(Part)
     if not Part:IsA("BasePart") then return end
 
     pcall(function()
-        shared.CG_LOW_GFX_CACHE[Part] = nil
+        shared.CG_LOW_GFX_PART_CACHE[Part] = nil
     end)
 end))
 
 function lowGfxModule.setLowGfx(lowGfxBool)
-    for Part, Properties in pairs(shared.CG_LOW_GFX_CACHE) do
+    for Part, Properties in pairs(shared.CG_LOW_GFX_PART_CACHE) do
         setProperty(Part, "Material", lowGfxBool and Enum.Material.SmoothPlastic or Properties.Material)
         setProperty(Part, "Reflectance", lowGfxBool and 0 or Properties.Reflectance)
         setProperty(Part, "CastShadow", lowGfxBool and false or Properties.CastShadow)
