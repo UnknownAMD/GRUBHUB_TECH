92c207254c6413d066b3020628e51cd45b413912
_Ben <54240316+botdevXD@users.noreply.github.com>
2024-06-19 17:31:38 +0100

    Update CG_LOW_GFX_MODULE.lua

--- CG_LOW_GFX_MODULE.lua
+++ CG_LOW_GFX_MODULE.lua
@@ -1,5 +1,7 @@
 local lowGfxModule = {}
 
+local Lighting = game:GetService("Lighting")
+
 shared.CG_LOW_GFX_CONNECTIONS = shared.CG_LOW_GFX_CONNECTIONS or {}
 
 for _, Connection in pairs(shared.CG_LOW_GFX_CONNECTIONS) do
@@ -7,9 +9,6 @@ for _, Connection in pairs(shared.CG_LOW_GFX_CONNECTIONS) do
 end
 
 table.clear(shared.CG_LOW_GFX_CONNECTIONS)
-
-shared.CG_LOW_GFX_LIGHTING_CACHE = shared.CG_LOW_GFX_LIGHTING_CACHE or {}
-shared.CG_LOW_GFX_PART_CACHE = shared.CG_LOW_GFX_PART_CACHE or {}
 
 local function getProperty(obj, propName)
     local success, value = pcall(function()
@@ -24,6 +23,17 @@ local function setProperty(obj, propName, value)
         obj[propName] = value
     end)
 end
+
+
+shared.CG_LOW_GFX_LIGHTING_CACHE = shared.CG_LOW_GFX_LIGHTING_CACHE or {
+    GlobalShadows = { Original = getProperty(Lighting, "GlobalShadows"), SwappedVersion = false },
+    ShadowSoftness = { Original = getProperty(Lighting, "ShadowSoftness"), SwappedVersion = 0 },
+    Technology = { Original = getProperty(Lighting, "Technology"), SwappedVersion = Enum.Technology.Voxel },
+    FogEnd = { Original = getProperty(Lighting, "FogEnd"), SwappedVersion = 9e9 },
+    FogStart = { Original = getProperty(Lighting, "FogStart"), SwappedVersion = 9e9 },
+}
+
+shared.CG_LOW_GFX_PART_CACHE = shared.CG_LOW_GFX_PART_CACHE or {}
 
 local function addToPartCache(Part)
     if typeof(Part) ~= "Instance" or not Part:IsA("BasePart") then return end
@@ -61,6 +71,10 @@ function lowGfxModule.setLowGfx(lowGfxBool)
         setProperty(Part, "Material", lowGfxBool and Enum.Material.SmoothPlastic or Properties.Material)
         setProperty(Part, "Reflectance", lowGfxBool and 0 or Properties.Reflectance)
         setProperty(Part, "CastShadow", lowGfxBool and false or Properties.CastShadow)
+    end
+
+    for Property, Data in pairs(shared.CG_LOW_GFX_LIGHTING_CACHE) do
+        setProperty(Lighting, Property, lowGfxBool and Data.SwappedVersion or Data.Original)
     end
 end
 
