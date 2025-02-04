31c2e224077b0d4032398f72800b3be516278eed
_Ben <54240316+botdevXD@users.noreply.github.com>
2025-02-04 21:43:25 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -317,7 +317,53 @@ getfenv().debug.info = getrenv().debug.info
 
 getfenv().require = getgenv().abc_require
 
-getgenv().WebSocket = nil
-
 --getgenv().saveinstance = loadstring(httpget("https://raw.githubusercontent.com/botdevXD/UniversalSynSaveInstance/refs/heads/main/saveinstance.luau", true))()
 --getgenv().Drawing = loadstring(httpget("https://raw.githubusercontent.com/botdevXD/GRUBHUB_TECH/main/DrawingLib.lua", true))()
+
+-- CREDIT: lovrewe
+local API: string = "http://api.plusgiant5.com"
+
+local last_call = 0
+local function call(konstantType: string, scriptPath: Script | ModuleScript | LocalScript): string
+    local success: boolean, bytecode: string = pcall(getscriptbytecode, scriptPath)
+
+    if (not success) then
+        return `-- Failed to get script bytecode, error:\n\n--[[\n{bytecode}\n--]]`
+    end
+
+    local time_elapsed = os.clock() - last_call
+    if time_elapsed <= .5 then
+        task.wait(.5 - time_elapsed)
+    end
+    local httpResult = request({
+        Url = API .. konstantType,
+        Body = bytecode,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "text/plain"
+        },
+    })
+    last_call = os.clock()
+    
+    if (httpResult.StatusCode ~= 200) then
+        return `-- Error occured while requesting the API, error:\n\n--[[\n{httpResult.Body}\n--]]`
+    else
+        return httpResult.Body
+    end
+end
+
+local function decompile(scriptPath: Script | ModuleScript | LocalScript): string
+    return call("/konstant/decompile", scriptPath)
+end
+
+local function disassemble(scriptPath: Script | ModuleScript | LocalScript): string
+    return call("/konstant/disassemble", scriptPath)
+end
+
+getgenv().decompile = decompile
+getgenv().disassemble = disassemble
+
+local synSI = loadstring(game:HttpGet("https://raw.githubusercontent.com/luau/SynSaveInstance/main/saveinstance.luau", true), "saveinstance")()
+
+getgenv().synsaveinstance = synSI
+getgenv().saveinstance = synSI
